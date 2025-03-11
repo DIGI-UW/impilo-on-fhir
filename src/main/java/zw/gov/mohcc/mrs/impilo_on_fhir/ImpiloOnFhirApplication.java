46684dfafd8046c8e6d03d9bef5b9c7b3c1859e4
HerbertYiga <herbertyiga24@egmail.com>
2025-03-11 15:34:45 +0300

    Initial commit

---
+++ src/main/java/zw/gov/mohcc/mrs/impilo_on_fhir/ImpiloOnFhirApplication.java
@@ -0,0 +1,13 @@
+package zw.gov.mohcc.mrs.impilo_on_fhir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImpiloOnFhirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImpiloOnFhirApplication.class, args);
+	}
+
+}
---
+++ src/test/java/zw/gov/mohcc/mrs/impilo_on_fhir/ImpiloOnFhirApplicationTests.java
@@ -0,0 +1,13 @@
+package zw.gov.mohcc.mrs.impilo_on_fhir;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImpiloOnFhirApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
